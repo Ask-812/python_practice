d3171942ee4c0a977e31b72d1c084627ff69c35d
Ask-812 <arnavsk812@gmail.com>
2023-08-18 21:10:04 +0530

    Really Important Logic

--- find-a-string.py
+++ find-a-string.py
@@ -1,9 +1,15 @@
 def count_substring(string, sub_string):
-    return
+    counter = 0
+    for i in range(len(string)-len(sub_string)+1):
+        temp = string[i:i+len(sub_string)]
+        print(temp)
+        if temp == sub_string:
+            counter += 1
+    return counter
 
 if __name__ == '__main__':
-    line = input().strip()
     string = input().strip()
+    sub_string = input().strip()
     
-    count = count_substring(line, string)
+    count = count_substring(string, sub_string)
     print(count)
