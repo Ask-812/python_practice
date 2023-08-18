f47e9a227b823a219e0f66d7a84ab000bc3f69bf
Ask-812 <arnavsk812@gmail.com>
2023-08-18 14:01:40 +0530

    modifying files

--- find-a-string.py
+++ find-a-string.py
@@ -2,8 +2,8 @@ def count_substring(string, sub_string):
     return
 
 if __name__ == '__main__':
+    line = input().strip()
     string = input().strip()
-    sub_string = input().strip()
     
-    count = count_substring(string, sub_string)
+    count = count_substring(line, string)
     print(count)
